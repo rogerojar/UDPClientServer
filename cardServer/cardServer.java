67fa9c3e5f07bcdbba81d12d0731314319bfc95c
rogerojar <44146275+rogerojar@users.noreply.github.com>
2019-02-23 15:21:53 -0400

    Add files via upload

---
+++ cardServer/cardServer.java
@@ -0,0 +1,27 @@
+/*
+Name : Roger Ojar
+ID : 809000518
+Assignment 1
+ */
+
+package javaapplication2;
+
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Roger
+ */
+public class cardServer{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        cardServerUI form = new cardServerUI();
+                form.setVisible(true);
+    }
+    
+}
---
+++ cardcounterudpclient/CardCounterUDPClient.java
@@ -0,0 +1,23 @@
+/*
+Name : Roger Ojar
+ID : 809000518
+Assignment 1
+ */
+package cardcounterudpclient;
+
+/**
+ *
+ * @author Roger
+ */
+public class CardCounterUDPClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        cardCounterUDPClientForm form = new cardCounterUDPClientForm();
+        form.setVisible(true);
+    }
+    
+}
